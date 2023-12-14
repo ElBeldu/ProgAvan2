c9d26d2a52ba7ed5634811906d5eff742da833f9
Timoteo Ezequiel Zazerej <137728959+ElBeldu@users.noreply.github.com>
2023-12-14 20:28:25 -0300

    Add files via upload

---
+++ ProgAvan/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ProgAvan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblTimeleft = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.Color.Transparent;
+            this.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblStatus.Font = new System.Drawing.Font("Segoe UI Black", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblStatus.Location = new System.Drawing.Point(729, 333);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(441, 42);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Emparejada o Desemparejada";
+            this.lblStatus.Click += new System.EventHandler(this.lblStatus_Click);
+            // 
+            // lblTimeleft
+            // 
+            this.lblTimeleft.AutoSize = true;
+            this.lblTimeleft.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimeleft.Location = new System.Drawing.Point(722, 28);
+            this.lblTimeleft.Name = "lblTimeleft";
+            this.lblTimeleft.Size = new System.Drawing.Size(247, 37);
+            this.lblTimeleft.TabIndex = 1;
+            this.lblTimeleft.Text = "Time Left  :  60";
+            this.lblTimeleft.Click += new System.EventHandler(this.lblTimeleft_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.Transparent;
+            this.btnRestart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRestart.Font = new System.Drawing.Font("Segoe UI Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestart.Location = new System.Drawing.Point(729, 468);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(246, 74);
+            this.btnRestart.TabIndex = 2;
+            this.btnRestart.Text = "Reiniciar";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.RestartGameEvent);
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Interval = 1000;
+            this.GameTimer.Tick += new System.EventHandler(this.TimerEvent);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(721, 142);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(133, 45);
+            this.scoreLabel.TabIndex = 3;
+            this.scoreLabel.Text = "Puntaje";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.lblTimeleft);
+            this.Controls.Add(this.lblStatus);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Memo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblTimeleft;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label scoreLabel;
+    }
+}
+
---
+++ ProgAvan/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgAvan
+{
+    public partial class Form1 : Form
+    {
+
+        List<int> numbers = new List<int> { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8 };
+        string firstChoice;
+        string secondChoice;
+        int tries;
+        List<PictureBox> pictures = new List<PictureBox>();
+        PictureBox picA;
+        PictureBox picB;
+        int totaltime = 60;
+        int countDownTime;
+        bool gameOver = false;
+        private int score = 0;
+        private bool match;
+    
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPictures();
+
+        }
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+            countDownTime--;
+
+            lblTimeleft.Text = "Tiempo restante : " + countDownTime;
+
+            if (countDownTime < 1)
+            {
+                GameOver("Tiempo terminado! .  Perdiste  ");
+
+                foreach (PictureBox x in pictures)
+                {
+                    if (x.Tag != null)
+                    {
+                        x.Image = Image.FromFile("pics/" + (string)x.Tag + ".png");
+                    }
+                }
+
+            }
+
+        }
+
+        private void RestartGameEvent(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+        private void LoadPictures()
+        {
+            int leftPos = 80;
+            int topPos = 80;
+            int rows = 0;
+
+            for (int i = 0; i < 16; i++)
+            {
+                PictureBox newPic = new PictureBox();
+                newPic.Height = 100;
+                newPic.Width = 100;
+                newPic.BackColor = Color.LightGray;
+                newPic.SizeMode = PictureBoxSizeMode.StretchImage;
+                newPic.Click += NewPic_Click;
+                pictures.Add(newPic);
+
+                if (rows < 4)
+                {
+                    rows++;
+                    newPic.Left = leftPos;
+                    newPic.Top = topPos;
+                    this.Controls.Add(newPic);
+                    leftPos = leftPos + 140;
+                }
+
+                if (rows == 4)
+                {
+                    leftPos = 80;
+                    topPos += 140;
+                    rows = 0;
+
+                }
+
+                RestartGame();
+
+
+            }
+
+        }
+        private void NewPic_Click(object sender, EventArgs e)
+        {
+            if (gameOver)
+            {
+                // No permitir el click si se termina el juego
+                return;
+            }
+            if (firstChoice == null)
+            {
+                picA = sender as PictureBox;
+                if (picA.Tag != null && picA.Image == null)
+                {
+                    picA.Image = Image.FromFile("pics/" + (string)picA.Tag + ".png");
+                    firstChoice = (string)picA.Tag;
+                }
+            }
+            else if (secondChoice == null)
+            {
+                picB = sender as PictureBox;
+                if (picB.Tag != null && picB.Image == null)
+                {
+                    picB.Image = Image.FromFile("pics/" + (string)picB.Tag + ".png");
+                    secondChoice = (string)picB.Tag;
+                }
+                // Para comprobar si coinciden
+                match = firstChoice == secondChoice;
+                if (match)
+                {
+                    score += 20;
+                }
+                else
+                {
+                    score -= 2;
+                }
+
+                // Actualizar la interfaz de usuario para mostrar el nuevo puntaje
+                scoreLabel.Text = "Puntaje: " + score;
+            }
+            else
+            {
+                CheckPictures(picA, picB);
+            }
+
+        }
+
+
+
+
+
+        private void RestartGame()
+        {
+            var randomList = numbers.OrderBy(X => Guid.NewGuid()).ToList();
+
+            numbers = randomList;
+
+            for (int i = 0; i < pictures.Count; i++)
+            {
+                pictures[i].Image = null;
+                pictures[i].Tag = numbers[i].ToString();
+            }
+            tries = 0;
+            lblStatus.Text = "Erradas : " + tries + " veces .";
+            lblTimeleft.Text = "Tiempo restante : " + totaltime;
+            gameOver = false;
+            GameTimer.Start();
+            countDownTime = totaltime;
+
+
+        }
+
+        private void CheckPictures(PictureBox A, PictureBox B)
+        {
+
+            if (firstChoice == secondChoice)
+            {
+                A.Tag = null;
+                B.Tag = null;
+                
+              
+            }
+            else
+            {
+                tries++;
+                lblStatus.Text = "Fallaste  " + tries + "   veces.";
+            }
+            firstChoice = null;
+            secondChoice = null;
+
+            foreach (PictureBox pics in pictures.ToList())
+            {
+                if (pics.Tag != null)
+                {
+                    pics.Image = null;
+                }
+            }
+
+            if (pictures.All(o => o.Tag == pictures[0].Tag))
+            {
+                GameTimer.Stop();
+                GameOver(" Buen trabajo. Has terminado ");
+                MessageBox.Show("  Presiona para jugar de nuevo  ");
+
+            }
+        }
+        
+            private void GameOver(string msg)
+        {
+            GameTimer.Stop();
+            gameOver = true;
+            MessageBox.Show(msg + "  Presiona para jugar de nuevo  ");
+            
+        }
+
+        private void lblTimeleft_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartGame(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+   
+}
